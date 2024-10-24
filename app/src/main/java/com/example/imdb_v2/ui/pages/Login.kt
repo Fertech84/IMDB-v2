3f5c1eba57c699771362108af08e5dd8cf39470e
Juan Fernando Martinez Dominguez <juan.md@globant.com>
2024-10-24 08:51:04 -0500

    signup page designed

--- app/src/main/java/com/example/imdb_v2/ui/pages/Login.kt
+++ app/src/main/java/com/example/imdb_v2/ui/pages/Login.kt
@@ -2,9 +2,7 @@ package com.example.imdb_v2.ui.pages
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,17 +13,11 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Label
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -51,9 +43,7 @@ import com.example.imdb_v2.ui.theme.mainYellow
 import org.w3c.dom.Text
 
 
-enum class TextFieldType {
-    Email, Password
-}
+
 
 @Composable
 fun LoginPage() {
